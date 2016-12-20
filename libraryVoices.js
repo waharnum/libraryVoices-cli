447ef1055c4ffb165b90003c80dbbd5e321bbe78
Alan Harnum <waharnum@gmail.com>
2016-12-20 08:14:29 -0500

    Refactoring.

--- libraryVoices.js
+++ libraryVoices.js
@@ -8,20 +8,24 @@ var ca = fluid.registerNamespace("ca");
 
 fluid.defaults("ca.alanharnum.libraryVoices", {
     gradeNames: "fluid.component",
-    wsOptions: {
+    config: {
         endpoint: "ws://45.55.209.67:4571/rtsearches"
     },
     listeners: {
         "onCreate.start": {
             funcName: "ca.alanharnum.libraryVoices.start",
-            args: ["{that}.options.wsOptions.endpoint"]
+            args: ["{that}.options.config.endpoint", "{that}"]
         }
     }
 });
 
-ca.alanharnum.libraryVoices.start = function (endpoint) {
-    // ca.alanharnum.libraryVoices.speakMode();
-    ca.alanharnum.libraryVoices.logMode(endpoint);
+ca.alanharnum.libraryVoices.start = function (endpoint, that) {
+    that.socket = new WS(endpoint);
+    that.socket.on('open', function open() {
+        console.log("Connection opened");
+    });
+    ca.alanharnum.libraryVoices.logMode(that.socket);
+    ca.alanharnum.libraryVoices.speakMode(that.socket);
 };
 
 ca.alanharnum.libraryVoices.logToFile = function (message) {
@@ -31,30 +35,18 @@ ca.alanharnum.libraryVoices.logToFile = function (message) {
     });
 };
 
-ca.alanharnum.libraryVoices.speakMode = function (endpoint) {
-    var socket = new WS(endpoint);
-
-    socket.on('open', function open() {
-        console.log("Connection opened");
-    });
-
+ca.alanharnum.libraryVoices.speakMode = function (socket) {
     socket.on('message', function(data, flags) {
-        console.log("Message received");
+        console.log("Message received, speaking message");
         console.log(data);
         var terms = JSON.parse(data)[0].terms;
         Say.speak(terms);
     });
 };
 
-ca.alanharnum.libraryVoices.logMode = function (endpoint) {
-    var socket = new WS(endpoint);
-
-    socket.on('open', function open() {
-        console.log("Connection opened");
-    });
-
+ca.alanharnum.libraryVoices.logMode = function (socket) {
     socket.on('message', function(data, flags) {
-        console.log("Message received");
+        console.log("Message received, logging message to file");
         console.log(data);
         var terms = JSON.parse(data)[0].terms;
         ca.alanharnum.libraryVoices.logToFile(terms);
